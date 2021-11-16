8494c55d17d15fbada6e4dc0d8b57fbdce376b17
LordNik10 <90309270+LordNik10@users.noreply.github.com>
2021-11-16 19:01:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,94 @@
+function showDaily()
+{
+    /*Work*/
+    document.getElementById('hoursw').textContent = '5hrs';
+    document.getElementById('prw').textContent = 'Last Week - 7 hrs';
+    
+    /*Play*/
+    document.getElementById('hoursp').textContent = '1hrs';
+    document.getElementById('prp').textContent = 'Last Week - 2 hrs';
+
+    /*Study*/
+    document.getElementById('hoursst').textContent = '0hrs';
+    document.getElementById('prst').textContent = 'Last Week - 1 hrs';
+
+    /*Excercise*/
+    document.getElementById('hourse').textContent = '1hrs';
+    document.getElementById('pre').textContent = 'Last Week - 1 hrs';
+
+    /*Social*/
+    document.getElementById('hoursso').textContent = '1hrs';
+    document.getElementById('prso').textContent = 'Last Week - 3 hrs';
+
+    /*Selfcare*/
+    document.getElementById('hourssc').textContent = '0hrs';
+    document.getElementById('prsc').textContent = 'Last Week - 1 hrs';
+
+    document.getElementById('d').style.color='white';
+    document.getElementById('m').style.color=null;
+    document.getElementById('w').style.color=null;
+
+}
+
+function showWeekly()
+{
+    /*Work*/
+    document.getElementById('hoursw').textContent = '32hrs';
+    document.getElementById('prw').textContent = 'Last Week - 36 hrs';
+    
+    /*Play*/
+    document.getElementById('hoursp').textContent = '10hrs';
+    document.getElementById('prp').textContent = 'Last Week - 8 hrs';
+
+    /*Study*/
+    document.getElementById('hoursst').textContent = '4hrs';
+    document.getElementById('prst').textContent = 'Last Week - 7 hrs';
+
+    /*Excercise*/
+    document.getElementById('hourse').textContent = '4hrs';
+    document.getElementById('pre').textContent = 'Last Week - 5 hrs';
+
+    /*Social*/
+    document.getElementById('hoursso').textContent = '5hrs';
+    document.getElementById('prso').textContent = 'Last Week - 10 hrs';
+
+    /*Selfcare*/
+    document.getElementById('hourssc').textContent = '2hrs';
+    document.getElementById('prsc').textContent = 'Last Week - 2 hrs';
+
+    document.getElementById('w').style.color='white';
+    document.getElementById('d').style.color=null;
+    document.getElementById('m').style.color=null;
+
+}
+
+function showMonthly()
+{
+    /*Work*/
+    document.getElementById('hoursw').textContent = '103hrs';
+    document.getElementById('prw').textContent = 'Last Week - 128 hrs';
+    
+    /*Play*/
+    document.getElementById('hoursp').textContent = '23hrs';
+    document.getElementById('prp').textContent = 'Last Week - 29 hrs';
+
+    /*Study*/
+    document.getElementById('hoursst').textContent = '13hrs';
+    document.getElementById('prst').textContent = 'Last Week - 19 hrs';
+
+    /*Excercise*/
+    document.getElementById('hourse').textContent = '11hrs';
+    document.getElementById('pre').textContent = 'Last Week - 18 hrs';
+
+    /*Social*/
+    document.getElementById('hoursso').textContent = '21hrs';
+    document.getElementById('prso').textContent = 'Last Week - 23 hrs';
+
+    /*Selfcare*/
+    document.getElementById('hourssc').textContent = '7hrs';
+    document.getElementById('prsc').textContent = 'Last Week - 11 hrs';
+
+    document.getElementById('m').style.color='white';
+    document.getElementById('d').style.color=null;
+    document.getElementById('w').style.color=null;
+}
